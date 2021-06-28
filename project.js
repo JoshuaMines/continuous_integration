99c4987f88d38658a7d524a76c5860551621fb3e
Joshua Mines <minesjoshua@gmail.com>
2021-06-28 15:31:20 -0600

    final

--- project.js
+++ project.js
@@ -3,10 +3,3 @@ module.exports = getGreeting, add;
 function getGreeting(name){
     return "Hi " + name;
 }
-
-
-
-add: function(num1, num2) {
-    return(num1 + num2);
-
-}
--- test.js
+++ test.js
@@ -1,10 +1,5 @@
-const getGreeting, add = require('./project');
+const getGreeting = require('./project');
 
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
-
-test('can we add 2 numbers together', () => {
-  expect(add(1, 2)).toBe(3);
-});
-
